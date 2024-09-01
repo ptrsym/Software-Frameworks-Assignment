2ed2d707007fb813b7317a315cc736de8bb8cdf7
ptrsym <ptr.sym@gmail.com>
2024-09-02 02:36:34 +1000

    update route structure. update service structure

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,28 @@
 const express = require('express');
+const path = require('path');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
+
+const http = require('http').Server(app);
+
+//handle cross origin requests
 app.use(cors());
+
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('server is running');
+//serve static content from angular build dir
+app.use(express.static(path.join(__dirname, '../frontend/dist/frontend/browser')));
+
+//redirect catch all to angular entry point
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend/dist/frontend/browser.index.html'))
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log('server running on port ${PORT}');
+let server = http.listen(PORT, () => {
+    let host = server.address().address;
+    let port = server.address().port;
+    console.log("Server listening on: "+ host +" port: " + port);
 });
